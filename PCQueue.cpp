9f986201ee7105b7e7cf6d558c97f5f371432ed3
Adi Mazuz <mazuz0992@gmail.com>
2018-12-20 15:12:32 +0200

    moved semophore to hpp file

--- PCQueue.cpp
+++
@@ -1,6 +0,0 @@
-//
-// Created by Dima on 19/12/2018.
-#include "PCQueue.hpp"
-
-//
-
--- Semaphore.hpp
+++ Semaphore.hpp
@@ -5,13 +5,23 @@
 // Synchronization Warm up 
 class Semaphore {
 public:
-	Semaphore(); // Constructs a new semaphore with a counter of 0
-	Semaphore(unsigned val); // Constructs a new semaphore with a counter of val
+    Semaphore():counter(0),cond(),mutex(){}
+    Semaphore(unsigned val):counter(val),cond(),mutex(){}
 
+    void down(){
+        pthread_mutex_lock(&mutex);
+        while (counter <= 0)
+            pthread_cond_wait(&cond,&mutex);
+        counter--;
+        pthread_mutex_unlock(&mutex);
+    }
 
-	void up(); // Mark: 1 Thread has left the critical section
-	void down(); // Block untill counter >0, and mark - One thread has entered the critical section.
-
+    void up(){
+        pthread_mutex_lock(&mutex);
+        pthread_cond_signal(&cond);
+        counter++;
+        pthread_mutex_unlock(&mutex);
+    }
 private:
 	int counter;
 	pthread_cond_t cond;
