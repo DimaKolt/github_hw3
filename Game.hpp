ca3ef4d36adbed1a3edca0b93b5237bbf8b30dcf
DIMA\Dima <dima_klt@yahoo.com>
2018-12-22 17:09:18 +0200

    RDY TO START

--- Game.hpp
+++ Game.hpp
@@ -21,7 +21,7 @@ struct game_params {
 class Game {
 public:
 
-	Game(game_params);
+	Game(game_params); //TODO init param to class parameters, but before to understant the N and the M size of given matrix. allocate 2 matrix on heap (curr & next)
 	~Game();
 	void run(); // Runs the game
 	const vector<float> gen_hist() const; // Returns the generation timing histogram  
@@ -31,7 +31,7 @@ public:
 
 protected: // All members here are protected, instead of private for testing purposes
 
-	// See Game.cpp for details on these three functions
+	// See Game.cpp for details on these three functions //TODO
 	void _init_game(); 
 	void _step(uint curr_gen); 
 	void _destroy_game(); 
@@ -40,16 +40,23 @@ protected: // All members here are protected, instead of private for testing pur
 	uint m_thread_num; 			 // Effective number of threads = min(thread_num, field_height)
 	vector<float> m_tile_hist; 	  // Shared Timing history for tiles: First m_thread_num cells are the calculation durations for tiles in generation 1 and so on.
 
-    inline void print_board(const char* header);
+    inline void print_board(const char* header); //TODO this func after we know N and M of matrix
 
     // Note: In your implementation, all m_thread_num threads must write to this structure.
 	vector<float> m_gen_hist;  	 // Timing history for generations: x=m_gen_hist[t] iff generation t was calculated in x microseconds
-	vector<Thread*> m_threadpool; // A storage container for your threads. This acts as the threadpool.
-
+	vector<Thread*> m_threadpool; // A storage container for your threads. This acts as the threadpool. //TODO init here (from _init func) vector of threads
+	
+	
 	bool interactive_on; // Controls interactive mode - that means, prints the board as an animation instead of a simple dump to STDOUT 
 	bool print_on; // Allows the printing of the board. Turn this off when you are checking performance (Dry 3, last question)
 
 	// TODO: Add in your variables and synchronization primitives
-
+	
+	int height_matrix;
+	int width_matrix;
+	PCQueue<ThrTask> tasks_q; //TODO ThrTask
+	//TODO add 2 heap feilds curr & next
+	
+	
 };
 #endif
